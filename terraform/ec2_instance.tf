cbd22ab38e8ebec3686c8a0522e3f4f3da8596be
khpahmil <pahmil@pusilkom.com>
2025-05-07 16:49:03 +0700

    add config terraform

--- terraform/ec2_instance.tf
+++ terraform/ec2_instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "example" {
-  ami           = "ami-00197bff829c798a8AMI"yang sesuai di region kamu
+  ami           = "ami-00197bff829c798a8AMI"
   instance_type = "t2.micro"
 
-  key_name = "pahmilama key pair SSH yang telah dibuat di AWS"
+  key_name = "pahmil"
 
   subnet_id = aws_subnet.subnet.id
   security_group = aws_security_group.allow_ssh_http.id
