0ed2e42860c7b1a1a39a91125574b6a3884448f9
khpahmil <pahmil@pusilkom.com>
2025-05-07 16:58:31 +0700

    add config terraform

--- terraform/ec2_instance.tf
+++ terraform/ec2_instance.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "example" {
-  ami           = "ami-00197bff829c798a8AMI"
+  ami           = "ami-00197bff829c798a8AMI"  # Pastikan AMI ID ini valid untuk region kamu
   instance_type = "t2.micro"
 
-  key_name = "pahmil"
+  key_name      = "pahmil"  # Pastikan key pair ini sudah ada di AWS EC2
 
-  subnet_id = aws_subnet.subnet.id
-  security_groups= aws_security_group.allow_ssh_http.id
+  subnet_id     = aws_subnet.subnet.id
+  security_groups = [aws_security_group.allow_ssh_http.id]  # Memperbaiki dengan menggunakan list
 
   tags = {
     Name = "example-instance"
